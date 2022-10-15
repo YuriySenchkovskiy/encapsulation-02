7869a637fe80c51659f2c9720e80957131c653a1
Yuriy <yuriy.senchkovskiy@gmail.com>
2022-10-15 15:24:44 +0300

    solution-01

--- encapsulation-02/Program.cs
+++ encapsulation-02/Program.cs
@@ -1,36 +1,199 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace encapsulation_02
 {
     internal class Program
-    {
+    { 
         public static void Main(string[] args)
         {
+            Store _store = new Store();
+            
+            _store.AddGoodsToWarehouse(new Good("IPhone 12"), 10);
+            _store.AddGoodsToWarehouse(new Good("IPhone 11"), 1);
+            _store.ShowWarehouse();
+            _store.AddToCart(new Good("IPhone 12"), 4);
+            
+            _store.AddToCart(new Good("IPhone 11"), 3);
+            _store.ShowCart();
+            _store.MakeOrder();
+            _store.ShowWarehouse();
+            
+            _store.AddGoodsToWarehouse(new Good("IPhone 12"), 10);
+            _store.ShowWarehouse();
+            _store.AddToCart(new Good("IPhone 12"), 9);
+            _store.ShowCart();
+            
+            _store.MakeOrder();
+            _store.ShowWarehouse();
+        }
+    }
+
+    public struct Good
+    {
+        public string Name { get; private set; }
+
+        public Good(string name)
+        {
+            Name = name;
+        }
+    }
+
+    public class Warehouse
+    {
+        private readonly Dictionary<Good, uint> _goods = new Dictionary<Good, uint>();
+        public IReadOnlyDictionary<Good, uint> Goods => _goods;
+
+        public void Deliver(Good good, uint number)
+        {
+            if (_goods.ContainsKey(good))
+            {
+               _goods[good] += number;
+            }
+            else
+            {
+                _goods.Add(good, number);
+            }
+        }
+
+        public void Subtract(Good key, uint value)
+        {
+            _goods[key] -= value;
+        }
+
+        public void ShowGoods()
+        {
+            Console.WriteLine("In warehouse:");
+            
+            foreach (var good in _goods)
+            {
+                Console.WriteLine($"Good name: {good.Key.Name}, good volume: {good.Value}");
+            }
+
+            if (_goods.Count == 0)
+                Console.WriteLine("You don't have any goods in warehouse");
+        }
+    }
+
+    public class Shop
+    {
+        private Warehouse _warehouse;
+        
+        public Shop(Warehouse warehouse)
+        {
+            _warehouse = warehouse;
+        }
+
+        public Cart GetCart()
+        {
+            return new Cart(_warehouse.Goods);
+        }
+
+        public void Subtract(Dictionary<Good, uint> cartGoods)
+        {
+            foreach (var good in cartGoods)
+                _warehouse.Subtract(good.Key, good.Value);
+        }
+    }
+
+    public class Cart
+    {
+        private Dictionary<Good, uint> _goodsInCart = new Dictionary<Good, uint>();
+        private Dictionary<Good, uint> _currentGoodsInWarehouse;
+
+        public IReadOnlyDictionary<Good, uint> GoodInCart => _goodsInCart;
+
+        public Cart(IReadOnlyDictionary<Good, uint> goods)
+        {
+            _currentGoodsInWarehouse = (Dictionary<Good, uint>)goods;
+        }
+
+        public void ShowGoods()
+        {
+            Console.WriteLine("Your order in cart:");
+            
+            foreach (var good in _goodsInCart)
+                Console.WriteLine($"Good name: {good.Key.Name}, good volume: {good.Value}");
+
+            if (_goodsInCart.Count == 0)
+                Console.WriteLine("You don't have any goods in cart");
+        }
+
+        public void Add(Good good, uint number)
+        {
+            if (!_currentGoodsInWarehouse.ContainsKey(good))
+            {
+                Console.WriteLine($"Incorrect good. We don't have {good.Name}. " +
+                                  $"Please, connect with us and let us know about this issues");
+                throw new ArgumentNullException();
+            }
+            
+            if (_currentGoodsInWarehouse[good] < number)
+            {
+                Console.WriteLine($"You're trying to order {number} units {good.Name}");
+                Console.WriteLine($"Now we have {good.Name} only {_currentGoodsInWarehouse[good]} units");
+                Console.WriteLine("Please, try again later");
+                throw new ArgumentOutOfRangeException();
+            }
+
+            _goodsInCart.Add(good, number);
+        }
+
+        public void Order()
+        {
+            Console.WriteLine("Paylink");
         }
     }
 
     public class Store
     {
-        Good iPhone12 = new Good("IPhone 12");
-        Good iPhone11 = new Good("IPhone 11");
+        private Warehouse _warehouse;
+        private Shop _shop;
+        private Cart _cart;
 
-        Warehouse warehouse = new Warehouse();
+        public Store()
+        {
+            _warehouse = new Warehouse();
+            _shop = new Shop(_warehouse);
+            SetCart();
+        }
 
-        Shop shop = new Shop(warehouse);
+        public void AddGoodsToWarehouse(Good good, uint number)
+        {
+            _warehouse.Deliver(good, number);
+            SetCart();
+        }
 
-        warehouse.Delive(iPhone12, 10);
-        warehouse.Delive(iPhone11, 1);
+        public void ShowWarehouse()
+        {
+            _warehouse.ShowGoods();
+        }
 
-//Вывод всех товаров на складе с их остатком
+        public void AddToCart(Good good, uint number)
+        {
+            _cart.Add(good, number);
+        }
 
-        Cart cart = shop.Cart();
-        cart.Add(iPhone12, 4);
-        cart.Add(iPhone11, 3); //при такой ситуации возникает ошибка так, как нет нужного количества товара на складе
+        public void ShowCart()
+        {
+            _cart.ShowGoods();
+        }
 
-//Вывод всех товаров в корзине
+        public void MakeOrder()
+        {
+            var goods = _cart.GoodInCart;
+            
+            if (goods.Count == 0)
+                return;
+            
+            _shop.Subtract((Dictionary<Good, uint>)goods);
+            _cart.Order();
+            SetCart();
+        }
 
-        Console.WriteLine(cart.Order().Paylink);
-
-        cart.Add(iPhone12, 9); //Ошибка, после заказа со склада убираются заказанные товары
+        private void SetCart()
+        {
+            _cart = _shop.GetCart();
+        }
     }
 }
